62a57998b2dba5bcd4fa8905cbe441971c69990f
Madalin Popa <contact@madalinpopa.com>
2024-02-22 19:11:23 +0200

    Update course enroll form

--- students/forms.py
+++ students/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from courses.models import Course
 
 
-class CourseEnrollForm(forms.form):
+class CourseEnrollForm(forms.Form):
     course = forms.ModelChoiceField(
         queryset=Course.objects.all(),
         widget=forms.HiddenInput,
