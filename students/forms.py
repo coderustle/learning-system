50a3fb52dcc155fef336eefb0c31338bdd0ad2af
Madalin Popa <contact@madalinpopa.com>
2024-02-22 18:07:45 +0200

    Add enroll course field

---
+++ students/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from courses.models import Course
+
+
+class CourseEnrollForm(forms.form):
+    course = forms.ModelChoiceField(
+        queryset=Course.objects.all(),
+        widget=forms.HiddenInput,
+    )
